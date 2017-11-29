357168fd1b94b1174b2b701674a262b4e0614864
ibramuppal <ibram.uppal@gmail.com>
2017-11-29 07:26:46 +0400

    t_time.py edited

--- t_time.py
+++ t_time.py
@@ -1,9 +1,25 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 
-hello_constant = tf.constant('Hello World!')
+#hello_constant = tf.constant('Hello World!')
+#with tf.Session() as sess:
+#    output = sess.run(hello_constant)
+#    print(output)
+
+# this is how you create an input field in Tensorflow
+# not the tf.string part but the tf.placeholder part 
+# the tf.string part is just the type of placeholder that this specific case is
+
+x = tf.placeholder(tf.string)
+y = tf.placeholder(tf.int32)
+z = tf.placeholder(tf.float32)
+
 
 with tf.Session() as sess:
-    output = sess.run(hello_constant)
+    #output = sess.run(x, feed_dict={x: 'Hello World' })
+    output = sess.run([x, y, z], feed_dict={x: 'Test String', y:123, z: 45.67}) 
     print(output)
+    
 
 
