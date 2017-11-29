ff18ff421b1520c24002ca44142dc277002a7ae0
ibramuppal <ibram.uppal@gmail.com>
2017-11-29 07:26:01 +0400

    adding first HOML book examples for tensor

---
+++ understand_tensorflow_I.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+x = tf.Variable(3, name="x")
+y = tf.Variable(4, name="4")
+
+f = x*x*y + y + 2
+
+sess = tf.Session()
+sess.run(x.initializer)
+sess.run(y.initializer)
+result = sess.run(f)
+
+print(result)
+sess.close()
+
